e079d57a3f28dbd79bb862b73358d909aeb5212a
Tobias Sargeant <toby@localhost.localdomain>
2012-08-29 12:34:27 +1000

    Add includes required for earlier versions of boost/ublas (for CentOS 5.8)

--- mst/mst/spanning_tree_clustering.cpp
+++ mst/mst/spanning_tree_clustering.cpp
@@ -27,7 +27,9 @@
 
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/symmetric.hpp>
+#include <boost/numeric/ublas/vector_proxy.hpp>
 #include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <boost/numeric/ublas/triangular.hpp>
 #include <boost/numeric/ublas/io.hpp>
 #include <boost/numeric/ublas/lu.hpp>
 
