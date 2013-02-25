04c5b0c23ace2f64abd1e4dbc2eac0f63cfc15a9
Tobias Sargeant <sargeant@wehi.edu.au>
2013-02-25 19:43:47 +1100

    Script for converting MSigDB xml file into mistic geneset format.

---
+++ extras/extract_msigdb_genesets.py
@@ -0,0 +1,34 @@
+import lxml.etree
+import lxml.etree
+import sys
+import json
+
+def intOrNone(x):
+    if x == '':
+        return None
+    return int(x)
+
+doc = lxml.etree.parse(open('msigdb_v3.1.xml'))
+root = doc.getroot()
+
+msigdb_info = open('info.txt', 'w')
+msigdb_entrez_id = open('entrez_id.txt', 'w')
+msigdb_official_symbol = open('official_symbol.txt', 'w')
+
+for geneset in root.xpath('/MSIGDB/GENESET'):
+
+    print >>msigdb_info, geneset.get('SYSTEMATIC_NAME'), json.dumps(dict(
+        name = geneset.get('STANDARD_NAME'),
+        desc = geneset.get('DESCRIPTION_BRIEF'),
+        pmid = intOrNone(geneset.get('PMID')),
+        org = geneset.get('ORGANISM'),
+        cat = '/'.join([ x for x in [ geneset.get('CATEGORY_CODE'), geneset.get('SUB_CATEGORY_CODE') ] if x ])
+    ))
+
+    print >>msigdb_entrez_id, geneset.get('SYSTEMATIC_NAME'), json.dumps(dict(
+        ids =geneset.get('MEMBERS_EZID').split(','),
+    ))
+
+    print >>msigdb_official_symbol, geneset.get('SYSTEMATIC_NAME'), json.dumps(dict(
+        ids =geneset.get('MEMBERS_SYMBOLIZED').split(','),
+    ))
