6e39ee92b588c5f3580db95bf0a4d4fb60e83ca5
Tobias Sargeant <sargeant@wehi.edu.au>
2013-11-11 12:57:41 +1100

    clean up trailing whitespace

--- mistic/app/static/js/lib/pairplot.js
+++ mistic/app/static/js/lib/pairplot.js
@@ -11,7 +11,7 @@
         };
 
         this.svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
- 
+
         this.width = this.options.width;
         this.height = this.options.height;
 
@@ -45,7 +45,7 @@
         }
     };
 
-   
+
     pairplot.prototype.removeData = function(matcher) {
         this.data = _.reject(this.data, matcher);
         this.draw();
@@ -56,10 +56,10 @@
         this.data.push(data);
         this.draw();
     };
-    
-   
+
+
     pairplot.prototype.draw = function() {
-       
+
         var svg = d3.select(this.svg)
 
         svg.selectAll('*').remove();
@@ -71,9 +71,9 @@
         var N = this.data.length;
         var x, y;
         var xlo, xhi, ylo, yhi;
-        
+
         var n_axis = 5;
-        
+
         var s_opts = {
             inner: 3,
             outer: 5,
@@ -91,7 +91,7 @@
 
         if (this.options.axes && N < n_axis) {
             _.extend(s_opts, { padding: [ 5,20,46,50 ], pt_size: 11*1/N});
-          
+
         } else {
             _.extend(s_opts, { padding: [ 5,5,5,5 ], pt_size: 1.5});
             sep = 5;
@@ -105,39 +105,39 @@
                 yhi = Math.floor(this.options.padding[0] + s_h * (y+1) / N - sep);
 
                 var g = svg.append('g').attr('transform', 'translate(' + String(xlo) + ',' + String(ylo) +')');
-              
-                
-                if (x != y) {  
+
+
+                if (x != y) {
                     _.extend(s_opts, { width: xhi - xlo, height: yhi - ylo });
-                   
-                    if (x < y ) {  _.extend(s_opts, {textOnly:true });} 
+
+                    if (x < y ) {  _.extend(s_opts, {textOnly:true });}
                     if (x > y ) {  _.extend(s_opts, {textOnly:false });}
                     var d = x-y;
-                     
-                    _.extend(s_opts, {axes:((d==1 && N < n_axis) ? true : false)}); 
-                    
-                    var s = new scatterplot(s_opts, this.data[x], this.data[y]); 
+
+                    _.extend(s_opts, {axes:((d==1 && N < n_axis) ? true : false)});
+
+                    var s = new scatterplot(s_opts, this.data[x], this.data[y]);
                     var result = s.svg;
-  
+
                     $(g[0]).append(result);
-                } 
-                
-                else {                  
+                }
+
+                else {
                     //console.log(JSON.stringify(this.data[x].symbol));
-                    
+
                      var fsize_symbol = 25-N;
                      var name_length = this.data[x].name.length;
                      var name = this.data[x].name  ?  this.data[x].name  : this.data[x].gene;
                      var name_space = this.width/(N)-this.options.padding[3]*N*2 - 50;
-                   
-                     if ((name_space)<name_length) { 
+
+                     if ((name_space)<name_length) {
                         if ((name_space)>3) {   name = name.slice(1,name_space)+'...';}
                         else {name = '';}
-                     } 
-                     
+                     }
+
                      var fsize_name = 16-N;
-                     
-                    
+
+
                      g.append('text')
                         .attr('x', (xhi-xlo)/2)
                         .attr('y', (yhi-ylo)/5-8)
@@ -146,7 +146,7 @@
                         .attr('style', 'font-family: helvetica; font-size: ' + fsize_symbol + 'px; font-weight: 600')
                         .attr('id', 'text-symbol')
                         .text(this.data[x].symbol ? this.data[x].symbol : this.data[x].gene);
-                     
+
                      if (name!='') {
                      g.append('text')
                         .attr('x', (xhi-xlo)/2)
@@ -154,31 +154,31 @@
                         .attr('dy', '12px')
                         .attr('text-anchor', 'middle')
                         .attr('style', 'font-family: helvetica; font-size: '+ fsize_name +'px; font-weight: 600')
-                        .attr('id', 'text-name') 
+                        .attr('id', 'text-name')
                         .text(name);
-                     }    
-  
+                     }
+
                      var fsize = 16-N, d=40, p=15;
-                      
-                     if (N>4){  p=11, d= 38;}                       
-                     if (name=='') {d = d-10;}  
-                     
+
+                     if (N>4){  p=11, d= 38;}
+                     if (name=='') {d = d-10;}
+
                      var expr = _.map(this.data[x].data, function(d) {return d.expr});
-                   
+
                      var sd_e = stats.stdev(expr);
                      var mu_e = stats.average(expr);
                      var rg_e = stats.range(expr);
-                     
+
                      if (fsize >= 10) {
-                     
+
                      g.append('text')
                         .attr('x', (xhi-xlo)/2)
                         .attr('y', (yhi-ylo)/5+d)
                         .attr('text-anchor', 'middle')
                         .attr('style', 'font-family: helvetica; font-size: '+fsize+'px; font-weight: 600')
                         .text('Mean =  ' + mu_e.toFixed(2));
-                     
-                     
+
+
                      g.append('text')
                         .attr('x', (xhi-xlo)/2)
                         .attr('y', (yhi-ylo)/5+d+p)
@@ -186,25 +186,25 @@
                         .attr('style', 'font-family: helvetica; font-size: '+fsize+'px; font-weight: 600')
                         .text('Std =  ' + sd_e.toFixed(2));
                      }
-                     
+
                     else { p=0; fsize=11;}
-                     
+
                      g.append('text')
                         .attr('x', (xhi-xlo)/2)
                         .attr('y', (yhi-ylo)/5+d+2*p)
                         .attr('text-anchor', 'middle')
                         .attr('style', 'font-family: helvetica; font-size: '+fsize+'px; font-weight: 600')
                         .text(' [' + rg_e[0].toFixed(2) +", "+ rg_e[1].toFixed(2)+"]");
-                       
-                   
+
+
                 }
             }
         }
-        
-     
+
+
     };
-      
-        
+
+
 })();
 
 
